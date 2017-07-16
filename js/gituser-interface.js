e71fa9330a86826d5e942da1fd07989f46d39464
derrickwaweru <derrickwaweru38@gmail.com>
2017-07-16 12:58:01 +0300

    added the github user api

--- js/gituser-interface.js
+++ js/gituser-interface.js
@@ -1,11 +1,31 @@
-var username = $('#ghusername').val();
-    var requri   = 'https://api.github.com/users/'+username;
-    var repouri  = 'https://api.github.com/users/'+username+'/repos';
+// this is for export//
+var Repos = require('./../js/github.js').reposModule;
+
+//this is a code used to display user within the website//
+var displayUser = function(user) {
+  // users.forEach(function(user) {
+  $('ul#userPic').empty();
+  $('ul#userPic').append("<li>" + user.name + "</li > ");
+  // });
+};
+
+//this is a code used to display repos within the website//
+var displayData = function(repos) {
+  $('ul#repoUrl').empty();
+  repos.forEach(function(repo) {
+    $('ul#repoUrl').append("<li><a href='" + repo.html_url + "'>" + repo.name + "</a>: " + repo.description + "; created on " + repo.created_at + "</li > ");
+  });
+};
+
+// front-end part for getting users and repos details//
 $(document).ready(function() {
-  var currentWeatherObject = new Weather();
-  $('#weatherLocation').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    currentWeatherObject.getWeather(city, displayHumidity);
+  var searchUsers = new Repos();
+  $('#searchName').click(function() {
+    //get input value
+    var name = $('#userName').val();
+    // $('#userPic').val("");
+    console.log(name);
+    searchUsers.getUser(name, displayUser);
+    searchUsers.getRepos(name, displayData);
   });
 });
--- js/gituser.js
+++ js/gituser.js
@@ -1,14 +1,30 @@
+//used to access the apiKey
 var apiKey = require('./../.env').apiKey;
 
-Weather = function(){
-};
+// This is a function created to enhance user access to the prototype//
 
-Weather.prototype.getWeather = function(city, displayFunction) {
-  $.get('http://api.github.com/users' + city + '&appid=' + apiKey).then(function(response) {
-    displayFunction(name, response.main.humidity);
+// This is a function created to enhance repo access to the prototype//
+Repos = function() {
+
+};
+// This is a prototype to enable user access/
+Repos.prototype.getUser = function(name, displayFunction) {
+  $.get('https://api.github.com/users/' + name + '?access_token=' + apiKey).then(function(users) {
+    displayFunction(users);
   }).fail(function(error) {
-    $('.showWeather').text(error.responseJSON.message);
+    $('.showUser').text("This Username " + name + " is " + error.responseJSON.message + "." +
+      "Please Enter the Correct Username");
+  });
+};
+// This is a prototype to enable the repository access //
+Repos.prototype.getRepos = function(name, displayFunction) {
+  $.get('https://api.github.com/users/' + name + '/repos?access_token=' + apiKey).then(function(repos) {
+    displayFunction(repos);
+  }).fail(function(error) {
+    $('.showUser').text("This Username " + name + " is " + error.responseJSON.message + "." +
+      "Please Enter the Correct Userrepository");
   });
 };
 
-exports.weatherModule = Weather;
+//used for exports.getRepos = Repos;
+exports.reposModule = Repos;
